c18199046a0e0b420f803dd4a9ff70bde3114dc0
henryurlo <henryurlo@gmail.com>
2021-03-26 23:22:38 -0400

    first commit

---
+++ admin/products/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .views import ProductViewSet
+from django.urls import path
+
+urlpatterns = [
+    path('products', ProductViewSet.as_view({
+        'get': 'list',
+        'post': 'create'
+    })),
+    
+    path('products/<str:pk>', ProductViewSet.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'delete': 'destroy'
+    })),
+]
+
